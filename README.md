4813db8e91f5a4c2e1ee5a26a91ac4e03668178c
Kapilraj <kapilraj.kalleda78@gmail.com>
2024-01-30 09:40:38 -0800

    Webapp is ready

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # bostonhouse
+
+create new environment
+----
+conda create -p venv python==3.12 -y
+---
+
+Git Commands
+
+git add .
+git commit -m "commit message"
+git push origin main
+
+
--- app.py
+++ app.py
@@ -16,6 +16,7 @@ scaler=pickle.load(open('scaling.pkl','rb'))
 def home():
     return render_template('home.html')
 
+#Create predict api and test with postman
 @app.route('/predict_api',methods=['POST'])
 
 def predict_api():
@@ -26,6 +27,16 @@ def predict_api():
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+#create forms to give inputs for webapp
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in (request.form.values())]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction price is {}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
